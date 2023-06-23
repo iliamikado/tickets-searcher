f9b4059148d9bb6f61a4af212d87d3907bc5790f
iliamikado <iliamikado@gmail.com>
2023-06-23 23:29:53 +0300

    Cash in service

--- src/service/service.ts
+++ src/service/service.ts
@@ -30,22 +30,75 @@ export type Review = {
     avatar?: string
 }
 
+const initialCash = {
+    films: {} as Record<string, Film>,
+    cinemas: {} as Record<string, Cinema>,
+    filmsInCinema: {} as Record<string, Film[]>,
+    reviews: {} as Record<string, Review[]>,
+    gotAllFilms: false,
+    gotAllCinemas: false
+}
+
+let cash = initialCash;
+
+function updateCash() {
+    cash = initialCash;
+    setTimeout(() => {updateCash()}, 1000 * 60 * 60)    //update cash every hour
+}
+updateCash();
+
 export function getAllFilms(): Promise<Film[]> {
-    return getResource('movies');
+    if (cash.gotAllFilms) {
+        return Promise.resolve(Object.values(cash.films));
+    }
+    cash.gotAllFilms = true;
+    return getResource('movies').then((films: Film[]) => {
+        films.forEach(film => {
+            cash.films[film.id] = film;
+        })
+        return films;
+    });
 }
 
 export function getAllCinemas(): Promise<Cinema[]> {
-    return getResource('cinemas');
+    if (cash.gotAllCinemas) {
+        return Promise.resolve(Object.values(cash.cinemas));
+    }
+    cash.gotAllCinemas = true;
+    return getResource('cinemas').then((cinemas: Cinema[]) => {
+        cinemas.forEach(cinema => {
+            cash.cinemas[cinema.id] = cinema;
+        })
+        return cinemas;
+    });
 }
 
 export function getFilmsInCinema(cinemaId: string): Promise<Film[]> {
-    return getResource(`movies?cinemaId=${cinemaId}`);
+    if (cash.filmsInCinema[cinemaId] !== undefined) {
+        return Promise.resolve(cash.filmsInCinema[cinemaId]);
+    }
+    return getResource(`movies?cinemaId=${cinemaId}`).then((films: Film[]) => {
+        cash.filmsInCinema[cinemaId] = films;
+        return films;
+    });
 }
 
 export function getMovie(movieId: string): Promise<Film> {
-    return getResource(`movie?movieId=${movieId}`);
+    if (cash.films[movieId]) {
+        return Promise.resolve(cash.films[movieId]);
+    }
+    return getResource(`movie?movieId=${movieId}`).then((film: Film) => {
+        cash.films[movieId] = film;
+        return film;
+    });
 }
 
 export function getReviews(movieId: string): Promise<Review[]> {
-    return getResource(`reviews?movieId=${movieId}`);
+    if (cash.reviews[movieId] !== undefined) {
+        return Promise.resolve(cash.reviews[movieId]);
+    }
+    return getResource(`reviews?movieId=${movieId}`).then((reviews: Review[]) => {
+        cash.reviews[movieId] = reviews;
+        return reviews;
+    });
 }
